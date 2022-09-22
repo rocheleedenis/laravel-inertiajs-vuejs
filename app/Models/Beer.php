5c0240ae994d45a96787964817bf020ebbab93e1
Rochele Edenís <rochele.edenis@gmail.com>
2022-09-21 22:02:55 -0300

    Creating the models

---
+++ app/Models/Beer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Beer extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function style()
+    {
+        return $this->belongsTo(Style::class);
+    }
+
+    public function brewery()
+    {
+        return $this->belongsTo(Brewery::class);
+    }
+}
